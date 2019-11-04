165b6a08f686e6a5d7192005fbfa5b2e77ad41f1
Harikrishnangithub <Harikrishnangithub@gmail.com>
2019-11-04 16:51:22 +0530

    second branch second commit

--- src/com/my/git/test/SampleTest.java
+++ src/com/my/git/test/SampleTest.java
@@ -2,7 +2,7 @@ package com.my.git.test;
 
 public class SampleTest {
 public static void main(String[] args) {
-	System.out.println("My data 3");
+	
 	System.out.println("My data 2");
 	System.out.println("My data 1");
 	
